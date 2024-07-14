e7c46cbc8b09cc3738bb245ab1ed7c89ad4e4e4b
SaiPrasadRajuThangella <spr.workspace@gmail.com>
2024-07-15 03:36:26 +0530

    Final App

---
+++ public/script.js
@@ -0,0 +1,62 @@
+let socket = io();
+let username = "";
+
+// console.log(socket)
+const messageContainer = document.getElementById("Messages");
+const btn = document.getElementById("Join-chat");
+const usernameInput = document.getElementById("username-input");
+const form = document.getElementById("form");
+const chatroomContainer = document.getElementById("chatroom-container");
+const messageInput = document.getElementById("message-input");
+const sendButton = document.getElementById("send-button");
+const sentMessage = document.getElementById("sent");
+btn.addEventListener("click", (event) => {
+  event.preventDefault();
+  username = usernameInput.value;
+  if (!username) {
+    alert("Please enter the Username");
+  } else {
+    form.style.display = "none";
+    chatroomContainer.style.display = "block";
+  }
+});
+let message = "";
+
+sendButton.addEventListener("click", (e) => {
+  e.preventDefault;
+  sendMessage();
+});
+
+function sendMessage() {
+  let message = messageInput.value.trim();
+  if (message) {
+    socket.emit("messageSent", { message, username });
+    messageInput.value = "";
+  } else {
+    alert("Please enter message");
+  }
+}
+
+messageInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    sendMessage();
+  }
+});
+
+socket.on("messageSent", (data) => {
+  if (data.username === username) {
+    renderMessage(data, true);
+  } else {
+    renderMessage(data, false);
+  }
+  // console.log(data);
+});
+
+function renderMessage(data, mine) {
+  if (mine) {
+    messageContainer.innerHTML += `<div class="Message sent" id="UsernameProperty">${data.username}:</div> <div class="Message sent">${data.message}</div>`;
+  } else {
+    messageContainer.innerHTML += `<div class="Message recieved" id="UsernameProperty" >${data.username}:</div> <div class="Message recieved">${data.message}</div>`;
+  }
+}
---
+++ server.js
@@ -0,0 +1,26 @@
+    const express = require("express")
+
+    const app =express();
+    const http = require('http');
+    const { Server }  = require("socket.io") 
+    const PORT = 9000;
+
+    const server = http.createServer(app);  
+    const io = new Server(server)
+    // console.log(io)
+
+    io.on("connection",(socket)=>{
+        // console.log("new commection hapopped",socket.id)
+        socket.on("messageSent",(data)=>{
+            io.emit("messageSent",data)
+        })
+    })
+    app.use(express.static("public"))
+
+    server.listen(PORT,()=>{
+        console.log(`server has been running on http://localhost:${PORT} `)
+
+    }) 
+
+
+
